bd006ca288c143193ba559a7026115cc68acb845
kitsushadow <spikespiegel19@gmail.com>
2016-11-24 12:05:05 -0500

    Updating master b4 tearing it apart

---
+++ src/main/java/nmd/primal/forgecraft/common/tiles/TileFirebox.java
@@ -0,0 +1,14 @@
+package nmd.primal.forgecraft.common.tiles;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemStack;
+import nmd.primal.forgecraft.common.blocks.FireBox;
+
+/**
+ * Created by kitsu on 11/24/2016.
+ */
+public class TileFirebox extends ForgeCraftTile{
+    private ItemStack[] iMatrix = new ItemStack[0];
+    private String fireboxCustomName;
+
+}
