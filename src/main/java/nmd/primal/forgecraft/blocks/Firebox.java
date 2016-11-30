08262632f164008c039548cf7b2e25a5b933c436
Mohammad-Ali Minaie <mminaie@bstonetech.com>
2016-11-30 17:14:48 -0500

    Startig work on tiles but need to add ITILEPROVIDER or some interface like that

--- src/main/java/nmd/primal/forgecraft/blocks/Firebox.java
+++ src/main/java/nmd/primal/forgecraft/blocks/Firebox.java
@@ -3,12 +3,15 @@ package nmd.primal.forgecraft.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import nmd.primal.forgecraft.ModInfo;
+import nmd.primal.forgecraft.tiles.TileFirebox;
 
 /**
  * Created by kitsu on 11/26/2016.
@@ -20,6 +23,12 @@ public class Firebox extends Block {
         setUnlocalizedName(ModInfo.ForgecraftBlocks.FIREBOX.getUnlocalizedName());
         setRegistryName(ModInfo.ForgecraftBlocks.FIREBOX.getRegistryName());
         setCreativeTab(ModInfo.TAB_FORGECRAFT);
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World worldIn, int meta)
+    {
+        return new TileFirebox();
     }
 
     @Override
---
+++ src/main/java/nmd/primal/forgecraft/tiles/TileFirebox.java
@@ -0,0 +1,11 @@
+package nmd.primal.forgecraft.tiles;
+
+import net.minecraft.tileentity.TileEntity;
+
+/**
+ * Created by mminaie on 11/30/16.
+ */
+public class TileFirebox extends TileEntity {
+
+
+}
