6f75507c0eff5222890db67f51faa0659a5d6b47
kitsushadow <spikespiegel19@gmail.com>
2016-12-10 10:45:35 -0500

    update push script

--- push.sh
+++ push.sh
@@ -2,7 +2,8 @@
 
 clear
 git status
-git add --all
+git add --all :/
 IFS= read -r -p "Enter commit message: " com
-git commit -m "$com"
-git push
+git commit -m "${com}"
+git push nmd master
+git push github master
