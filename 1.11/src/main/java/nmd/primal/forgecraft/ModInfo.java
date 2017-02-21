e373b73f671474ea8b1085b0b0b839b676f463c9
Mohammad-Ali Minaie <mminaie@bstonetech.com>
2017-02-20 21:53:16 -0500

    push 1.1.12

--- 1.11/src/main/java/nmd/primal/forgecraft/ModInfo.java
+++ 1.11/src/main/java/nmd/primal/forgecraft/ModInfo.java
@@ -17,7 +17,7 @@ public class ModInfo {
     public static final String MOD_NAME = "Kitsu's ForgeCraft";
     //public static final String MOD_PREFIX = MOD_ID + ":";
     public static final String MOD_CHANNEL = MOD_ID;
-    public static final String MOD_VERSION = "1.1.11";
+    public static final String MOD_VERSION = "1.1.12";
     public static final String MC_VERSIONS = "[1.11.0, 1.12.0)";
 
     /** Mod Structures **/
