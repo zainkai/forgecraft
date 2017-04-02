be17c83fefe186655699435f4418722bb7941730
elite <elite.nmd.so>
2017-04-02 15:23:19 -0500

    local comit for merging

--- 1.11/src/main/java/nmd/primal/forgecraft/items/weapons/Musket.java
+++ 1.11/src/main/java/nmd/primal/forgecraft/items/weapons/Musket.java
@@ -1,8 +1,17 @@
 package nmd.primal.forgecraft.items.weapons;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import nmd.primal.forgecraft.ModInfo;
 import nmd.primal.forgecraft.items.BaseItem;
+
+import javax.annotation.Nullable;
 
 /**
  * Created by Lyle on 4/2/2017.
@@ -15,5 +24,12 @@ public class Musket extends BaseItem{
         this.setCreativeTab(ModInfo.TAB_FORGECRAFT);
         this.setMaxStackSize(1);
         this.setNoRepair();
+        this.addPropertyOverride(new ResourceLocation("type"), new IItemPropertyGetter() {
+            @SideOnly(Side.CLIENT)
+            public float apply(ItemStack item, @Nullable World worldIn, @Nullable EntityLivingBase entityIn) {
+                return 0.0f;
+
+            }
+        });
     }
 }
