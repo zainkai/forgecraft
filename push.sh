67e2f4a5f4b9ace9b56fedc5deebb0daf8d97cfa
Mohammad-Ali Minaie <mminaie@bstonetech.com>
2017-04-06 21:30:49 -0400

    modified push script

--- push.sh
+++ push.sh
@@ -5,7 +5,8 @@ git status
 git add --all :/
 IFS= read -r -p "Enter commit message: " com
 git commit -m "${com}"
-echo $(git branch)
+#echo $(git branch)
+git branch
 IFS= read -r -p "Enter Branch name: " com1
 git push nmd ${com1}
 git push github ${com1}
