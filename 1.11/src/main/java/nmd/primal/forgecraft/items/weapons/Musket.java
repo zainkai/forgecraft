f6306c568596c296614a2218ed1a9d2d3e7c7c2b
elite <elite.nmd.so>
2017-04-06 22:19:45 -0500

    "Positioning musket properly while shooting"

--- 1.11/src/main/java/nmd/primal/forgecraft/items/weapons/Musket.java
+++ 1.11/src/main/java/nmd/primal/forgecraft/items/weapons/Musket.java
@@ -3,9 +3,11 @@ package nmd.primal.forgecraft.items.weapons;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
+import net.minecraft.item.EnumAction;
 import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.stats.StatList;
 import net.minecraft.util.*;
 import net.minecraft.world.World;
@@ -33,34 +35,91 @@ public class Musket extends BaseItem{
         this.addPropertyOverride(new ResourceLocation("type"), new IItemPropertyGetter() {
             @SideOnly(Side.CLIENT)
             public float apply(ItemStack item, @Nullable World worldIn, @Nullable EntityLivingBase entityIn) {
-                return 0.0f;
+                if(item.hasTagCompound()){
+                    if ( item.getTagCompound().getBoolean("use") == true) {
+                        return 0.1f;
+                    } else return 0.0f;
+                } else return 0.0f;
 
             }
         });
     }
     // Temporary onItemRightClick method
     @Override
-    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand)
+    public void onPlayerStoppedUsing(ItemStack stack, World world, EntityLivingBase entityLiving, int timeLeft)
     {
-        ItemStack stack = player.getHeldItem(hand);
-        /*
-        if (!player.capabilities.isCreativeMode)
-        {
-            stack.shrink(1);
+        System.out.println("Stopped Using");
+        EntityPlayer player = (EntityPlayer) entityLiving;
+        System.out.println(player.getItemInUseCount());
+
+
+            //if(player.getItemInUseCount() > 5) {
+        world.playSound( null, player.posX, player.posY, player.posZ, ModSounds.MUSKET_SHOT, SoundCategory.BLOCKS, 0.5F, 0.3F / (itemRand.nextFloat() * 0.4F + 0.8F));
+        EntityMuckBall entity = new EntityMuckBall(world, player);
+        entity.setHeadingFromThrower(player, player.rotationPitch, player.rotationYaw, 0.0F, 7.0F, 0.5F);
+        world.spawnEntity(entity);
+        if (!stack.hasTagCompound()) {
+            stack.setTagCompound(new NBTTagCompound());
         }
-        */
+        stack.getTagCompound().setBoolean("use", false);
+
+        //}
 
 
 
-        if (!world.isRemote)
+    }
+/*
+    public ItemStack onItemUseFinish(ItemStack stack, World world, EntityLivingBase entityLiving)
+    {
+        if (entityLiving instanceof EntityPlayer)
         {
-            world.playSound((EntityPlayer)null, player.posX, player.posY, player.posZ, ModSounds.MUSKET_SHOT, SoundCategory.BLOCKS, 0.5F, 0.3F / (itemRand.nextFloat() * 0.4F + 0.8F));
+            EntityPlayer player = (EntityPlayer) entityLiving;
+            world.playSound( null, player.posX, player.posY, player.posZ, ModSounds.MUSKET_SHOT, SoundCategory.BLOCKS, 0.5F, 0.3F / (itemRand.nextFloat() * 0.4F + 0.8F));
             EntityMuckBall entity = new EntityMuckBall(world, player);
             entity.setHeadingFromThrower(player, player.rotationPitch, player.rotationYaw, 0.0F, 7.0F, 0.5F);
             world.spawnEntity(entity);
+
         }
 
-        player.addStat(StatList.getObjectUseStats(this));
-        return new ActionResult(EnumActionResult.SUCCESS, stack);
+        return stack;
+    }
+*/
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        ItemStack itemstack = playerIn.getHeldItem(handIn);
+        boolean flag = true;
+        if (!itemstack.hasTagCompound()) {
+            itemstack.setTagCompound(new NBTTagCompound());
+        }
+        itemstack.getTagCompound().setBoolean("use", true);
+
+        ActionResult<ItemStack> ret = net.minecraftforge.event.ForgeEventFactory.onArrowNock(itemstack, worldIn, playerIn, handIn, flag);
+        if (ret != null) {
+            return ret;
+        }
+
+        if (!playerIn.capabilities.isCreativeMode && !flag)
+        {
+            return flag ? new ActionResult(EnumActionResult.PASS, itemstack) : new ActionResult(EnumActionResult.FAIL, itemstack);
+        }
+        else
+        {
+            playerIn.setActiveHand(handIn);
+            return new ActionResult(EnumActionResult.SUCCESS, itemstack);
+        }
+    }
+
+    public int getMaxItemUseDuration(ItemStack stack)
+    {
+        return 7200;
+    }
+
+    /**
+     * returns the action that specifies what animation to play when the items is being used
+     */
+    public EnumAction getItemUseAction(ItemStack stack)
+    {
+        return EnumAction.BOW;
     }
 }
