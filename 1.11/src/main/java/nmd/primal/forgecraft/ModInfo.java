2764cae3e9bdb73a57d260ad83512fe38ce9f096
Mohammad-Ali Minaie <mminaie@bstonetech.com>
2017-04-23 14:26:25 -0400

    updating dependendency check and version

--- 1.11/src/main/java/nmd/primal/forgecraft/ModInfo.java
+++ 1.11/src/main/java/nmd/primal/forgecraft/ModInfo.java
@@ -16,9 +16,9 @@ public class ModInfo {
     public static final String MOD_NAME = "Kitsu's ForgeCraft";
     //public static final String MOD_PREFIX = MOD_ID + ":";
     public static final String MOD_CHANNEL = MOD_ID;
-    public static final String MOD_VERSION = "1.2.20";
+    public static final String MOD_VERSION = "1.2.21";
     public static final String MC_VERSIONS = "[1.11.0, 1.12.0)";
-    public static final String DEPENDENCIES = "required-after:forge@[13.20.0.2226,);" + "required-after:primal;";
+    public static final String DEPENDENCIES = "required-after:forge@[13.20.0.2226,);" + "required-after:primal@[0.3.15,);";
 
     /** Mod Structures **/
     public static final String SERVER_PROXY = "nmd.primal.forgecraft.proxy.ServerProxy";
